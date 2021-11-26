526f5b776963287541b39026ae3e1fbbdc35776c
lhmirage <1210871489@qq.com>
2021-11-26 17:00:50 +0800

    up

--- src/views/FooterMain.vue
+++ src/views/FooterMain.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="footer-main">
-
+    <div class="footer-content">
+      <p>pipimusic</p>
+      <p class="last-p">pipimusic</p>
+      <div class="link">
+        <a href="https://github.com/lhmirage">
+          <i class="iconfont icongithub"></i>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +18,32 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped lang="less">
+.footer-main{
+  width: 100%;
+  background-color: #161e27;
+  padding-top: 20px;
+  .footer-content{
+    border-bottom: 1px solid #797979;
+    p:first-child{
+      color: #fff;
+      padding: 30px 0 20px 20px;
+      border-left: 5px solid #fa2800;
+    }
+    .last-p{
+      padding: 10px;
+      color: #a9a9a9;
+    }
+    .link{
+      padding: 20px 0 20px 20px;
+      i{
+        font-size: 18px;
+        border: 10px solid #232a31;
+        border-radius: 50%;
+        margin-left: 10px;
+        color: #e7e7e7;
+      }
+    }
+  }
+}
 </style>
--- src/views/HeaderMain.vue
+++ src/views/HeaderMain.vue
@@ -10,24 +10,71 @@
       </div>
       <ul>
         <li>
-          <router-link to="/home" tag="a">发现音乐</router-link>
+          <router-link to="/home"
+                       tag="a">发现音乐</router-link>
         </li>
         <li>
-          <router-link to="/rank" tag="a">排行榜</router-link>
+          <router-link to="/rank"
+                       tag="a">排行榜</router-link>
         </li>
         <li>
-          <router-link to="/song_sheet" tag="a">歌单</router-link>
+          <router-link to="/song_sheet"
+                       tag="a">歌单</router-link>
         </li>
         <li>
-          <router-link to="/singer" tag="a">歌手</router-link>
+          <router-link to="/singer"
+                       tag="a">歌手</router-link>
         </li>
         <li>
-          <router-link to="/video" tag="a">视频</router-link>
+          <router-link to="/video"
+                       tag="a">视频</router-link>
         </li>
         <li>
-          <router-link to="/mv" tag="a">MV</router-link>
+          <router-link to="/mv"
+                       tag="a">MV</router-link>
         </li>
       </ul>
+      <div class="search">
+        <i class="iconfont iconsearch"
+           @click="openSearch"></i>
+      </div>
+      <div class="user">
+        <router-link to="/login"
+                     tag="a"
+                     v-if="!profile">登录</router-link>
+        <div v-if="profile"
+             class="profile-login">
+          <div class="avatar">
+            <img :src="profile.avatarUrl"
+                 alt="">
+          </div>
+          <div class="user-info">
+            <el-dropdown trigger="click"
+                         @command="handleCommand">
+              <span class="el-dropdown-link">
+                {{profile.nickname}}<i class="el-icon-arrow-down el-icon--right"></i>
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item command="profile-el">
+                  <i class="iconfont iconmine"></i>个人主页
+                </el-dropdown-item>
+                <el-dropdown-item>
+                  <i class="iconfont icondengji"></i>我的等级
+                </el-dropdown-item>
+                <el-dropdown-item>
+                  <i class="iconfont iconshezhi"></i>个人设置
+                </el-dropdown-item>
+                <el-dropdown-item command="sign-out">
+                  <i class="iconfont icontuichu"></i>退出登录
+                </el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="search-wrapper">
+
     </div>
   </div>
 </template>
